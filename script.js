cf2036be7e5c4085ff457b18aabc74f29069816d
AzharBasim <63117971+AzharBasim@users.noreply.github.com>
2020-10-01 00:18:16 +0700

    v1

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "viska diffana"&&b=="01102000")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
